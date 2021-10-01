2ebcd4acdffb5c4535b12ee168fed3222e795205
Empire <unmykx@gmail.com>
2021-10-01 13:59:27 -0700

    added class change function for keydown

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-
+// play audio and change class on keydown
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+    const key = document.querySelector(`.key[data-key="${e.code}"]`);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
-    console.log(audio)
+    key.classList.add('playing');
+    console.log(key)
 };
 
 
+// remove class change after trigger
+function removeTransition(e){
+    if( e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+
+// add event listener for each key
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
+// trigger audio and class change
 window.addEventListener('keydown', playSound);
